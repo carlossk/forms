593d4f62a79e146e2c2098269394e74b862f1e79
CarlosSK <carlos.acosta@cargosprint.com>
2019-10-19 01:45:06 -0500

    form by data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
@@ -7,7 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  public form1: FormGroup;
+  constructor() {
+    this.form1 = new FormGroup({
+      name: new FormControl('Carlos', [Validators.required,Validators.minLength(3)], []),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+      ])
+
+    });
+  }
+  saveData() {
+    console.log(this.form1.value);
+  }
 
   ngOnInit() {
   }
